4d0611ab5d11e55352947a2ae82c6503be47a587
iyeszin <iyeszin@gmail.com>
2023-05-09 17:01:01 +0200

    add load model

--- inference.py
+++ inference.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 import numpy as np
 import copy
 import cv2 as cv
+from keras import models
+from utils import helper
 
 class_names = ["A","B","C","D","E","F","G","H","I","K","L",'M','N','O','P','Q','R','S','T','U','V','W','X','Y']
 
@@ -16,23 +18,20 @@ N_IMAGES = 1
 
 INPUT_SHAPE = (28, 28, 1)
 NUMBER_OF_CLASSESS = 24
+MODEL_NAME = "/home/iyeszin/Documents/sl_recognizer/saved_models_res18/model.h5"
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-img", "--imagepath", type=str, default = "/home/iyeszin/Documents/sl_recognizer/a.jpg" , help="path to dataset")
+ap.add_argument("-img", "--imagepath", type=str, default = "/home/iyeszin/Documents/sl_recognizer/Images/20230220_084429.jpg" , help="path to dataset")
 ap.add_argument("-m", "--modeltype", type=str, default = "res34", help="to specific which model architecture to train, ie, cnn, res18, res34")
-ap.add_argument("-w", "--weight", type=str, default = "weight/res34-weight.hdf5", help="path to specific which model architecture to train")
 args = vars(ap.parse_args())
 
 img_path = args["imagepath"]
 model_type = args["modeltype"]
-weight = args["weight"]
-
-# print(model_type)
 
 class Inference():
    
-    def __init__(self):
+    def __init__(self, imgpath):
         super().__init__()
         self.IMAGE_WIDTH = 28
         self.IMAGE_HEIGHT = 28
@@ -41,16 +40,14 @@ class Inference():
         self.INPUT_SHAPE = (28, 28, 1)
         self.NUMBER_OF_CLASSESS = 24
         self.model_type = model_type
-        # self.cp_img = []
         self.pixels = []
-        # self.model = self.model
 
-        self.cp_img = self.getImgCallback()
+        self.cp_img = self.getImgCallback(imgpath)
         print(self.cp_img.size)
-        self.evaluate_model()
+        self.evaluate_model(self.cp_img)
 
-    def getImgCallback(self):
-        self.img = cv.imread(img_path)
+    def getImgCallback(self, imgpath):
+        self.img = cv.imread(imgpath)
         self.img = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
         print(self.img.size)
         self.img = cv.resize(self.img, [28, 28])
@@ -61,44 +58,19 @@ class Inference():
         return self.cp_img
 
 
-        # self.img = Image.open(img_path)
-        # print(self.img.size)
-        # self.img = self.img.resize((28,28))
-        # self.img = self.img.convert('L')
-        # self.img = self.img.rotate(-90)
-        # self.cp_img = copy.deepcopy(self.img)
-        # self.pixels = np.array(self.img.getdata()).reshape((1,28,28))/255
+    def evaluate_model(self, img):
 
-        # return pixels
-        # plt.imshow(pixels.reshape((28,28)),cmap='gray')
-
-
-    def evaluate_model(self):
-        if self.model_type == "res18":
-            self.model = Model.ResNet18(INPUT_SHAPE, NUMBER_OF_CLASSESS)
-        elif self.model_type == "res34":
-            self.model = Model.ResNet34(INPUT_SHAPE, NUMBER_OF_CLASSESS)
-        elif self.model_type == "cnn":
-            self.model = Model.cnn(INPUT_SHAPE, NUMBER_OF_CLASSESS)
-        else:
-            print("no model is load")
+        self.model = models.load_model(MODEL_NAME)
     
         #------ load data
-        # print("[INFO] Reading image...")
-        pixels = np.array(self.img).reshape((1,28,28))/255
-        print(pixels.size)
-        # plt.imshow(pixels.reshape((28,28)),cmap='gray')
+        print("[INFO] Reading image...")
+        pixels = helper.preprocess_image(img)
 
         # ------ inferenece
-        if self.model_type == "cnn":
-            pixels = pixels.reshape(N_IMAGES, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)
         my_images_preds = self.model.predict(pixels)
-        my_images_preds = np.argmax(my_images_preds)
+        label = np.argmax(my_images_preds)
         print(my_images_preds)
-        print("Predicted letter is: "+ class_names[my_images_preds])
-
-# def main(args=None):
-#     inference_img = Inference()
+        print("Predicted letter is: "+ class_names[label])
 
 if __name__ == '__main__':
-    inference_img = Inference()
+    inference_img = Inference(img_path)
