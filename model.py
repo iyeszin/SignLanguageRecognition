066f0a2f9c52974efecfe0724d8b8eeeed18ded8
iyeszin <iyeszin@gmail.com>
2023-05-09 15:45:20 +0200

    update model and add early stopping

--- model.py
+++ model.py
@@ -129,14 +129,16 @@ class Model:
         model = Sequential()
         
         model.add(Conv2D(32, (3, 3), activation='relu', input_shape=shape))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D(pool_size=(2, 2)))
         
+        model.add(Conv2D(32, (3, 3), activation='relu'))
         model.add(Conv2D(64, (3, 3), activation='relu'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(128, (3, 3), activation='relu'))
+        model.add(Conv2D(256, (3, 3), activation='relu'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D(pool_size=(2, 2)))
         
